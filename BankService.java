0aa17c2c93f6be0fe2d3221504a62b46aea336f4
Manoj <manojkumar.vsm55@gmail.com>
2026-01-30 06:27:43 +0530

    25000

--- BankService.java
+++ BankService.java
@@ -1,5 +1,5 @@
 class BankService {
-    int balance = 8000;   // initial balance
+    int balance = 25000;   // initial balance
 
     void deposit(int amount) {
         balance = balance + amount;
